cba8729198b130db0762e7738f67418ed20031d6
love-to-code108 <love.to.code108@gmail.com>
2025-05-06 09:38:24 +0530

    finally shadcn ui is working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,43 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
+// adding alert dialogue
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 function App() {
-
   return (
-    <div>
-      Hello world
-    </div>
-  )
+    <ThemeProvider>
+      <div>Hello world</div>
+      <ModeToggle />
+
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
